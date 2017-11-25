c340a0766df0caac86d790a04f2bcaa08e37318b
Aníbal Álvarez González <anibal.alvarezg@gmail.com>
2017-11-25 20:33:55 -0300

    jFrame maximizado al ejecutarse

--- SistemaRemuneraciones/src/Rem/RemGui.java
+++ SistemaRemuneraciones/src/Rem/RemGui.java
@@ -16,6 +16,8 @@ public class RemGui extends javax.swing.JFrame {
      */
     public RemGui() {
         initComponents();
+        setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+
     }
 
     /**
@@ -81,7 +83,10 @@ public class RemGui extends javax.swing.JFrame {
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
+        RemIFMAgregar agregarP = new RemIFMAgregar();
         
+       this.jDesktopPane1.add(agregarP);
+       agregarP.show();
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
