c9922d8e5beafa8b4656fe1cf3bcc6394783a580
Fabián Vergara <fabianes00@hotmail.com>
2017-11-28 00:49:14 -0300

    Pagos Multiples++

--- SistemaRemuneraciones/src/Rem/RemIFMPagoMultiple.java
+++ SistemaRemuneraciones/src/Rem/RemIFMPagoMultiple.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+
 /**
  *
  * @author Drebin01
@@ -60,12 +61,12 @@ public class RemIFMPagoMultiple extends javax.swing.JInternalFrame {
         jTableMostrar = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTableStack = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jButtonLimpiarLista = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton4 = new javax.swing.JButton();
+        fieldFechaPago = new javax.swing.JTextField();
+        jButtonEliminarDeLista = new javax.swing.JButton();
+        jButtonSeleccionarTodo = new javax.swing.JButton();
 
         setClosable(true);
         setTitle("Pagos Multiples");
@@ -111,17 +112,10 @@ public class RemIFMPagoMultiple extends javax.swing.JInternalFrame {
         });
         jScrollPane2.setViewportView(jTableStack);
 
-        jButton1.setText("Selccionar todo");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButtonLimpiarLista.setText("Limpiar lista");
+        jButtonLimpiarLista.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
-        jButton2.setText("Limpiar lista");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jButtonLimpiarListaActionPerformed(evt);
             }
         });
 
@@ -134,13 +128,25 @@ public class RemIFMPagoMultiple extends javax.swing.JInternalFrame {
 
         jLabel1.setText("Fecha de Pago:");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        fieldFechaPago.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                fieldFechaPagoActionPerformed(evt);
             }
         });
 
-        jButton4.setText("Eliminar de la lista");
+        jButtonEliminarDeLista.setText("Eliminar de la lista");
+        jButtonEliminarDeLista.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonEliminarDeListaActionPerformed(evt);
+            }
+        });
+
+        jButtonSeleccionarTodo.setText("Seleccionar todo");
+        jButtonSeleccionarTodo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSeleccionarTodoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -148,21 +154,21 @@ public class RemIFMPagoMultiple extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(128, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(jButton2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton4))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton3)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButtonSeleccionarTodo)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jButtonLimpiarLista)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButtonEliminarDeLista))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(fieldFechaPago, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3))))
                 .addGap(104, 104, 104))
         );
         layout.setVerticalGroup(
@@ -171,41 +177,46 @@ public class RemIFMPagoMultiple extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
+                .addComponent(jButtonSeleccionarTodo)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton4))
+                    .addComponent(jButtonLimpiarLista)
+                    .addComponent(jButtonEliminarDeLista))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton3)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(fieldFechaPago, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButtonLimpiarListaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLimpiarListaActionPerformed
         // TODO add your handling code here:
-        //TableModel model = jTableMostrar.getModel();
-        jTableStack.setModel(jTableMostrar.getModel());
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
+        
+        //DefaultTableModel model = (DefaultTableModel)jTableMostrar.getModel();
+        //model.setRowCount(0);
+        //mostrarListaPersonasInTable();
+    }//GEN-LAST:event_jButtonLimpiarListaActionPerformed
+    
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        DefaultTableModel modelStack = (DefaultTableModel)jTableStack.getModel();
+        String fechaPago   =   fieldFechaPago.getText();
+        //Object[] row = new Object[100];
+        for(int i = 0; i < modelStack.getRowCount(); i++){
+            //row[0] = modelStack.getValueAt(count, 1).toString()
+            System.out.println (modelStack.getValueAt(i, 1).toString());
+        }
     }//GEN-LAST:event_jButton3ActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void fieldFechaPagoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fieldFechaPagoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_fieldFechaPagoActionPerformed
 
     private void jTableMostrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableMostrarMouseClicked
         // TODO add your handling code here:
@@ -218,21 +229,43 @@ public class RemIFMPagoMultiple extends javax.swing.JInternalFrame {
         row[2]  = model.getValueAt(i,2).toString();
         row[3]  = model.getValueAt(i,3).toString();
         modelStack.addRow(row);
+        ((DefaultTableModel)jTableMostrar.getModel()).removeRow(i);
         //modelStack.addTableModelListener(jTableMostrar.getModel());
         //jTableStack.setModel(model);
     }//GEN-LAST:event_jTableMostrarMouseClicked
 
+    private void jButtonSeleccionarTodoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSeleccionarTodoActionPerformed
+        // TODO add your handling code here:
+        DefaultTableModel model = (DefaultTableModel)jTableMostrar.getModel();
+        jTableStack.setModel(model);
+        //model.setRowCount(0);
+    }//GEN-LAST:event_jButtonSeleccionarTodoActionPerformed
+
+    private void jButtonEliminarDeListaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEliminarDeListaActionPerformed
+        // TODO add your handling code here:
+        DefaultTableModel modelMostrar = (DefaultTableModel)jTableMostrar.getModel();
+        int i = jTableStack.getSelectedRow();
+        TableModel model = jTableStack.getModel();
+        Object[] row = new Object[4];
+        row[0]  = model.getValueAt(i,0).toString();
+        row[1]  = model.getValueAt(i,1).toString();
+        row[2]  = model.getValueAt(i,2).toString();
+        row[3]  = model.getValueAt(i,3).toString();
+        modelMostrar.addRow(row);
+        ((DefaultTableModel)jTableStack.getModel()).removeRow(i);
+    }//GEN-LAST:event_jButtonEliminarDeListaActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTextField fieldFechaPago;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButtonEliminarDeLista;
+    private javax.swing.JButton jButtonLimpiarLista;
+    private javax.swing.JButton jButtonSeleccionarTodo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTableMostrar;
     private javax.swing.JTable jTableStack;
-    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
